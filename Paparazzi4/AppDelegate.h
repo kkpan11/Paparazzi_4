bf014c005b4d6176dd8bf8e2b9e72492128e8fe6
Gianni <gianni@herxun.co>
2012-03-16 16:09:14 +0800

    Initial Commit

---
+++ Paparazzi4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paparazzi4
+//
+//  Created by  on 3/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
